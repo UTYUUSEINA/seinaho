d029531eb7278ad1939cabac623c9e32d01ec53e
seina <newskoihosi@yahoo.co.jp>
2023-07-17 21:40:54 +0900

    one

---
+++ src/Sseinadayonn.java
@@ -0,0 +1,14 @@
+
+public class Sseinadayonn {
+
+	public static void main(String[] args) {
+		System.out.println("dfdfdf");
+		System.out.println("dfdfdf");
+		System.out.println("dfdfdf");
+		System.out.println("dfdfdf");
+		System.out.println("dfdfdf");
+		
+
+	}
+
+}
